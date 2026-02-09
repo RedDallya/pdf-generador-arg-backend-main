27892e79d5f1342f1284b952de289a39e8564133
pipi <piaqasalas@gmail.com>
2026-02-09 10:31:31 -0300

    commit

--- controllers/client.controller.js
+++ controllers/client.controller.js
@@ -67,3 +67,23 @@ export const deleteClient = async (req, res) => {
     res.status(500).json({ error: "Error eliminando cliente" });
   }
 };
+
+export async function getClientById(req, res) {
+  try {
+    const { id } = req.params;
+
+    const [rows] = await pool.query(
+      "SELECT * FROM clientes WHERE id = ?",
+      [id]
+    );
+
+    if (!rows.length) {
+      return res.status(404).json({ error: "Cliente no encontrado" });
+    }
+
+    res.json(rows[0]);
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
--- routes/clients.routes.js
+++ routes/clients.routes.js
@@ -1,16 +1,21 @@
 import express from "express";
 import {
   getClients,
+  getClientById,
   createClient,
   updateClient,
   deleteClient
 } from "../controllers/client.controller.js";
 
+
+
 const router = express.Router();
 
 router.get("/", getClients);
+router.get("/:id", getClientById);
 router.post("/", createClient);
 router.put("/:id", updateClient);
 router.delete("/:id", deleteClient);
+
 
 export default router;
