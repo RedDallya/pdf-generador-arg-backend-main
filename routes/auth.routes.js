0d7e6ac08b094d118b82719acdc504d8638e1ad7
pipi <piaqasalas@gmail.com>
2026-02-12 19:00:17 -0300

    authroutes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,8 @@
 import express from "express";
-import { login, refresh, logout } from "../controllers/auth.controller.js";
+import { login } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/login", login);
-router.post("/refresh", refresh);
-router.post("/logout", logout);
 
 export default router;
