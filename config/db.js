ca93f397e493cf48ac441e205057002f56482372
pipi <piaqasalas@gmail.com>
2026-02-13 10:43:30 -0300

    sssssssssssssssssssssss

--- config/db.js
+++ config/db.js
@@ -1,16 +1,23 @@
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
-// Carga .env sólo en desarrollo
 if (process.env.NODE_ENV !== "production") {
   dotenv.config();
 }
 
+console.log("Conectando a DB con:", {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+});
+
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,      // Ej: mysql.railway.internal
-  user: process.env.DB_USER,      // Ej: root
-  password: process.env.DB_PASS,  // Ej: contraseña secreta
-  database: process.env.DB_NAME,  // Ej: railway
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
 });
